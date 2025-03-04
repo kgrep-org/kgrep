046ae01972c73734f4255c049427e2d30ead4c28
Helber Belmiro <helber.belmiro@gmail.com>
2025-03-04 11:48:30 -0300

    Added support for Service Accounts (#90)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- src/main/java/com/thegreatapi/kgrep/KgrepCommand.java
+++ src/main/java/com/thegreatapi/kgrep/KgrepCommand.java
@@ -3,6 +3,7 @@ package com.thegreatapi.kgrep;
 import com.thegreatapi.kgrep.configmap.ConfigMapsCommand;
 import com.thegreatapi.kgrep.log.LogsCommand;
 import com.thegreatapi.kgrep.pod.PodsCommand;
+import com.thegreatapi.kgrep.serviceaccount.ServiceAccountsCommand;
 import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine.Command;
 
@@ -10,7 +11,8 @@ import picocli.CommandLine.Command;
 @Command(name = "kgrep", mixinStandardHelpOptions = true, versionProvider = VersionProvider.class, subcommands = {
         LogsCommand.class,
         ConfigMapsCommand.class,
-        PodsCommand.class
+        PodsCommand.class,
+        ServiceAccountsCommand.class
 })
 class KgrepCommand {
 }
