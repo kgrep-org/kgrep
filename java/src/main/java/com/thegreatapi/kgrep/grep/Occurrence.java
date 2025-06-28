72bbb176e96f29532a8480b823025b2f12275ab0
Helber Belmiro <helber.belmiro@gmail.com>
2025-06-28 19:30:11 -0300

    Removed old Java code (#135)
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- java/src/main/java/com/thegreatapi/kgrep/grep/Occurrence.java
+++
@@ -1,4 +0,0 @@
-package com.thegreatapi.kgrep.grep;
-
-public record Occurrence(int lineNumber, String text) {
-}
--- java/src/main/java/com/thegreatapi/kgrep/resource/ResourceLine.java
+++
@@ -1,4 +0,0 @@
-package com.thegreatapi.kgrep.resource;
-
-public record ResourceLine(String resourceName, int lineNumber, String text) {
-}
