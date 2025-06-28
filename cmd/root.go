de5b366f8002150cb86b0ecb7125ec8a79750505
Helber Belmiro <helber.belmiro@gmail.com>
2025-06-28 19:52:15 -0300

    Updated release
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,23 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
+)
+
+// These variables are set at build time using ldflags
+var (
+	Version    = "dev"
+	BuildTime  = "unknown"
+	CommitHash = "unknown"
 )
 
 var rootCmd = &cobra.Command{
 	Use:     "kgrep",
 	Short:   "kgrep - Search and analyze logs and resources in Kubernetes",
 	Long:    `kgrep is a command-line utility designed to simplify the process of searching and analyzing logs and resources in Kubernetes.`,
-	Version: "0.1.0",
+	Version: Version,
 }
 
 func Execute() error {
@@ -17,4 +26,16 @@ func Execute() error {
 
 func init() {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+
+	// Add a custom version command that shows more details
+	versionCmd := &cobra.Command{
+		Use:   "version",
+		Short: "Show detailed version information",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("kgrep version %s\n", Version)
+			fmt.Printf("Build time: %s\n", BuildTime)
+			fmt.Printf("Commit hash: %s\n", CommitHash)
+		},
+	}
+	rootCmd.AddCommand(versionCmd)
 }
