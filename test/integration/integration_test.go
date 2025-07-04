9812f2445b83e3814e1bfa602f2b91f08b4cd08e
Helber Belmiro <helber.belmiro@gmail.com>
2025-07-04 12:18:34 -0300

    Added support for cluster-scoped resources like namespaces
    Signed-off-by: Helber Belmiro <helber.belmiro@gmail.com>

--- test/integration/integration_test.go
+++ test/integration/integration_test.go
@@ -698,3 +698,126 @@ func TestIntegration_CustomResourceSearchContent(t *testing.T) {
 		})
 	}
 }
+
+func TestIntegration_ClusterScopedResourceSearch(t *testing.T) {
+	clientset := setupKubernetesClient(t)
+	namespaceName := getTestNamespace(t)
+
+	createTestNamespace(t, clientset, namespaceName)
+	defer cleanupTestNamespace(t, clientset, namespaceName)
+
+	output, err := runKgrepCommand(t, "resources", "-k", "namespace", "-p", namespaceName)
+	assert.NoError(t, err)
+	assert.Contains(t, output, namespaceName, "Should find the test namespace")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "namespaces", "-p", namespaceName)
+	assert.NoError(t, err)
+	assert.Contains(t, output, namespaceName, "Should find the test namespace using plural form")
+}
+
+func TestIntegration_NamespacedResourceIsolation(t *testing.T) {
+	clientset := setupKubernetesClient(t)
+	namespace1 := getTestNamespace(t)
+	namespace2 := getTestNamespace(t)
+
+	createTestNamespace(t, clientset, namespace1)
+	createTestNamespace(t, clientset, namespace2)
+	defer func() {
+		cleanupTestNamespace(t, clientset, namespace1)
+		cleanupTestNamespace(t, clientset, namespace2)
+	}()
+
+	configMap1 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-config",
+			Namespace: namespace1,
+		},
+		Data: map[string]string{
+			"config": "unique-content-namespace1",
+		},
+	}
+
+	configMap2 := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-config",
+			Namespace: namespace2,
+		},
+		Data: map[string]string{
+			"config": "unique-content-namespace2",
+		},
+	}
+
+	_, err := clientset.CoreV1().ConfigMaps(namespace1).Create(context.Background(), configMap1, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	_, err = clientset.CoreV1().ConfigMaps(namespace2).Create(context.Background(), configMap2, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	output, err := runKgrepCommand(t, "resources", "-k", "configmap", "-n", namespace1, "-p", "unique-content-namespace1")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "unique-content-namespace1", "Should find content from namespace1")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "configmap", "-n", namespace1, "-p", "unique-content-namespace2")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "No occurrences", "Should not find content from namespace2 when searching in namespace1")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "configmap", "-n", namespace2, "-p", "unique-content-namespace2")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "unique-content-namespace2", "Should find content from namespace2")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "configmap", "-n", namespace2, "-p", "unique-content-namespace1")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "No occurrences", "Should not find content from namespace1 when searching in namespace2")
+}
+
+func TestIntegration_NonExistentNamespaceIsolation(t *testing.T) {
+	clientset := setupKubernetesClient(t)
+	realNamespace := getTestNamespace(t)
+	nonExistentNamespace := "non-existent-namespace-12345"
+
+	createTestNamespace(t, clientset, realNamespace)
+	defer cleanupTestNamespace(t, clientset, realNamespace)
+
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-config",
+			Namespace: realNamespace,
+		},
+		Data: map[string]string{
+			"config": "content-in-real-namespace",
+		},
+	}
+
+	_, err := clientset.CoreV1().ConfigMaps(realNamespace).Create(context.Background(), configMap, metav1.CreateOptions{})
+	require.NoError(t, err)
+
+	output, err := runKgrepCommand(t, "resources", "-k", "configmap", "-n", nonExistentNamespace, "-p", "content-in-real-namespace")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "No occurrences", "Should not find content from real namespace when searching in non-existent namespace")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "configmap", "-n", realNamespace, "-p", "content-in-real-namespace")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "content-in-real-namespace", "Should find content when searching in correct namespace")
+}
+
+func TestIntegration_ClusterScopedVsNamespacedBehavior(t *testing.T) {
+	clientset := setupKubernetesClient(t)
+	testNamespace := getTestNamespace(t)
+
+	createTestNamespace(t, clientset, testNamespace)
+	defer cleanupTestNamespace(t, clientset, testNamespace)
+
+	output, err := runKgrepCommand(t, "resources", "-k", "namespace", "-n", testNamespace, "-p", "default")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "default", "Should find default namespace even when specifying a different namespace for cluster-scoped resources")
+
+	createTestPod(t, clientset, testNamespace)
+
+	output, err = runKgrepCommand(t, "resources", "-k", "pod", "-n", testNamespace, "-p", "test-pod")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "test-pod", "Should find pod in specified namespace")
+
+	output, err = runKgrepCommand(t, "resources", "-k", "pod", "-n", "default", "-p", "test-pod")
+	assert.NoError(t, err)
+	assert.Contains(t, output, "No occurrences", "Should not find pod from test namespace when searching in default namespace")
+}
